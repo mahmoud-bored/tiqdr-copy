fa619bcf84f05435fdcca2f8a6ffa02c4aeffa3d
mahmoud-bored <83987531+mahmoud-bored@users.noreply.github.com>
2023-08-12 11:31:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,221 @@
+const cards = {
+    card1: {
+        title: "الأسبوع الأول",
+        sections: {
+            0:{
+                tag: ["file"],
+                title: "ملفات",
+                "ملف شرح الأسبوع الأول": "https://drive.google.com/file/d/1oH9snHZpBuCCn0XvlAFXGtHBa0URoXLc/view?usp=sharing",
+            },
+            1:{
+                tag: ["video", "video", "video", 'video'],
+                title: " محاضرة 1 | مهارات ما قبل التأسيس",
+                "المحاضرة التعريفية بالدورة": "https://www.youtube.com/embed/ITnJ6zwKdKM?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=1",
+                "محاضرة 1 تأسيس": "https://www.youtube.com/embed/fX6-HwX7-4I?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=2",
+                "تابع محاضرة 1": "https://www.youtube.com/embed/hLzGPzVJagg?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=3",
+                "حل واجب المحاضرة الأولى": "https://www.youtube.com/embed/39FcPC4crtA?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=4",
+            },
+            2:{
+                tag: ["video", 'video', 'video'],
+                title: "محاضرة 2 | مهارات ما قبل التأسيس",
+                "شرح المحاضرة 2": "https://www.youtube.com/embed/l-7M4IrT_Go?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=5",
+                "تابع شرح المحاضرة 2": "https://www.youtube.com/embed/p3XjvQd-1Y4?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=6",
+                "حل واجب المحاضرة 2": "https://www.youtube.com/embed/2OxLpga-Wt0?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=7",
+            },
+            3:{
+                tag: ["video", 'video'],
+                title: " محاضرة 3 | مهارات ما قبل التاسيس ",
+                "محاضرة 3 مهارات ما قبل التأسيس": "https://www.youtube.com/embed/6Eaw_WsbU_k?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=8",
+                "حل واجب محاضرة 3": "https://www.youtube.com/embed/druYX1qQpzg?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=9",
+            },
+            4:{
+                tag: ["video"],
+                title: "محاضرة 4 | مهارات ما قبل التأسيس",
+                "محاضرة 4 تعلم التبسيط والإختصارات والقسمة": "https://www.youtube.com/embed/7LUc9PeMNTY?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=11",
+            },
+            5:{
+                tag: ["video"],
+                title: "محاضرة 5 | ايجاد باقي القسمة",
+                "محاضرة 5 باقي القسمة": "https://www.youtube.com/embed/HJrYh3-500A?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=12"
+            },
+            6:{
+                tag: ['test', 'video'],
+                title: "اختبار 1 | الأسبوع الأول",
+                "اختبار الأسبوع 1": "#",
+                "حل اختبار الأسبوع 1": "https://www.youtube.com/embed/OA59-nNKxyY?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=2",
+            }
+        }
+    },
+    card2: {
+        title: "الأسبوع الثاني",
+        sections: {
+            0:{
+                tag: ["file"],
+                title: "ملفات",
+                "ملف شرح الأسبوع الثاني": "https://drive.google.com/file/d/1qAh77DFhG2rk79O9c0ruStKleYrXpobo/view?usp=sharing",
+            },
+            1:{
+                tag: ["video", 'video'],
+                title: "محاضرة 1 | حساب النسبة والنسبة المئوية",            
+                "محاضرة 1 حساب النسبة والنسبة المئوية": "https://www.youtube.com/embed/RsWFK83EQ5U?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=1",
+                "حل واجب المحاضرة 1": "https://www.youtube.com/embed/4KZVJcCiTzo?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=2",
+            },
+            2:{
+                tag: ["video", 'video'],
+                title: "محاضرة 2 | تمارين الربح و الخسارة و السعر الاصلي",
+                "محاضرة 2 تمارين الربح و الخسارة و السعر الاصلي": "https://www.youtube.com/embed/VL4r5ojpk7g?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=3",
+                "شرح واجب محاضرة 2": "https://www.youtube.com/embed/8lJ9p5qOkxs?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=4",
+            },
+            3:{
+                tag: ["video", 'video'],
+                title: "محاضرة 3 | اجزاء النسب و الضرب التبادلي",
+                "محاضرة 3 اجزاء النسب و الضرب التبادلي": "https://www.youtube.com/embed/nedPAOkORGk?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=5",
+                "حل واجب محاضرة 3": "https://www.youtube.com/embed/w0DUMLELYk4?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=6",
+            },
+            4:{
+                tag: ["video", 'video'],
+                title: "محاضرة 4 | الوسط الحسابي",
+                "محاضرة 4 الوسط الحسابي": "https://www.youtube.com/embed/Pc5wPN-3oDE?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=7",
+                "حل واجب محاضرة 4": "https://www.youtube.com/embed/PN3EcUCuw4I?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=8",
+            },
+            5:{
+                tag: ["video", 'video'],
+                title: "محاضرة 5 | الاحصاء و الاحتمالات",
+                "محاضرة 5 الاحصاء و الاحتمالات": "https://www.youtube.com/embed/B64LfpkEck0?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=9",
+                "حل واجب محاضرة 5": "https://www.youtube.com/embed/fMlMPU4hkD0?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=10",
+            },
+        }
+    },
+    card3: {
+        title: "الأسبوع الثالث",
+        sections: {
+            0:{
+                tag: [],
+                title: "NaN"
+            }
+        }
+    },
+    card4: {
+        title: "الأسبوع الرابع",
+        sections: {
+            0:{
+                tag: [],
+                title: "NaN"
+            }
+        }
+    },
+    card5: {
+        title: "الأسبوع الخامس",
+        sections: {
+            0:{
+                tag: [],
+                title: "NaN"
+            }
+        }
+    },
+    card6: {
+        title: "الأسبوع السادس",
+        sections: {
+            0:{
+                tag: [],
+                title: "NaN"
+            }
+        }
+    },
+    card7: {
+        title: "الأسبوع السابع",
+        sections: {
+            0:{
+                tag: [],
+                title: "NaN"
+            }
+        }
+    },
+    card8: {
+        title: "تجميعات شاملة",
+        sections: {
+            0:{
+                tag: [],
+                title: "NaN"
+            }
+        }
+    },
+}
+
+function createCards(cardsObj){
+    const main = document.querySelector('main')
+
+}
+function createNewCardContainer(cardInfo){
+    // Create Outer Container
+    const cardContainer = document.createElement('div')
+    cardContainer.classList.add('card-outer-container')
+    
+    // Create the Title inside the Outer Container
+    const titleContainer = document.createElement('div')
+    titleContainer.classList.add('title-container')
+    titleContainer.innerHTML = `
+        <h3>${cardInfo.title}</h3>
+    `
+    cardContainer.appendChild(titleContainer)
+    
+    // Create the Inner Container
+    const cardInnerContainer = document.createElement('div')
+    cardInnerContainer.classList.add('card-inner-container')
+    
+    // Create Sections inside the Inner Container
+    for(const section in cardInfo.sections){
+        
+        // New Section Element
+        const newSection = document.createElement('section')
+        newSection.classList.add('group-section-container')
+        
+        // Create Section Title
+        const newSectionTitle = document.createElement('h3')
+        newSectionTitle.classList.add('group-title')
+        newSectionTitle.textContent = `${cardInfo.sections[section].title}`
+        newSection.appendChild(newSectionTitle)
+        
+        // Create the Group Elements Container
+        const newSectionGroupContainer = document.createElement('div')
+        newSectionGroupContainer.classList.add('group-elements-container')
+        // Loop inside the Elements
+        let tagIndex = 0
+        for(elmnt in cardInfo.sections[section]){
+            if(elmnt != 'tag' && elmnt != 'title'){
+                // Create Group Element
+                const newGroupElement = document.createElement('div')
+                newGroupElement.classList.add('group-element')
+                newGroupElement.innerHTML = `
+                    <div class="group-element-tag">
+                        <img src="./assets/${cardInfo.sections[section].tag[tagIndex]}.png">
+                    </div>
+                    <a href="${cardInfo.sections[section][elmnt]}">
+                        <div class="group-element-text">${elmnt}</div>
+                    </a>
+                `
+                // Append Group Elmnt
+                newSectionGroupContainer.appendChild(newGroupElement)
+                tagIndex++
+            }
+        }
+        newSection.appendChild(newSectionGroupContainer)
+        cardInnerContainer.appendChild(newSection)
+
+        const newHrElmnt = document.createElement('hr')
+        cardInnerContainer.appendChild(newHrElmnt)
+    }
+    cardContainer.appendChild(cardInnerContainer)
+
+    return cardContainer
+}
+
+function appendContiners(){
+    const scrollContainer = document.createElement('div')
+    scrollContainer.classList.add('flex-container')
+    for (const card in cards) {
+        scrollContainer.appendChild(createNewCardContainer(cards[card]))
+    }
+    document.querySelector('main').appendChild(scrollContainer)
+}
+appendContiners()

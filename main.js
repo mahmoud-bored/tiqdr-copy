9c7e713815b55e6b32598390f260d3104f79fb5f
mahmoud-bored <83987531+mahmoud-bored@users.noreply.github.com>
2023-08-12 13:03:23 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,39 +10,39 @@ const cards = {
             1:{
                 tag: ["video", "video", "video", 'video'],
                 title: " محاضرة 1 | مهارات ما قبل التأسيس",
-                "المحاضرة التعريفية بالدورة": "https://www.youtube.com/embed/ITnJ6zwKdKM?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=1",
-                "محاضرة 1 تأسيس": "https://www.youtube.com/embed/fX6-HwX7-4I?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=2",
-                "تابع محاضرة 1": "https://www.youtube.com/embed/hLzGPzVJagg?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=3",
-                "حل واجب المحاضرة الأولى": "https://www.youtube.com/embed/39FcPC4crtA?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=4",
+                "المحاضرة التعريفية بالدورة": "https://www.youtube.com/watch?v=ITnJ6zwKdKM",
+                "محاضرة 1 تأسيس": "https://www.youtube.com/watch?fX6-HwX7-4I",
+                "تابع محاضرة 1": "https://www.youtube.com/watch?hLzGPzVJagg",
+                "حل واجب المحاضرة الأولى": "https://www.youtube.com/watch?39FcPC4crtA",
             },
             2:{
                 tag: ["video", 'video', 'video'],
                 title: "محاضرة 2 | مهارات ما قبل التأسيس",
-                "شرح المحاضرة 2": "https://www.youtube.com/embed/l-7M4IrT_Go?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=5",
-                "تابع شرح المحاضرة 2": "https://www.youtube.com/embed/p3XjvQd-1Y4?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=6",
-                "حل واجب المحاضرة 2": "https://www.youtube.com/embed/2OxLpga-Wt0?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=7",
+                "شرح المحاضرة 2": "https://www.youtube.com/watch?l-7M4IrT_Go",
+                "تابع شرح المحاضرة 2": "https://www.youtube.com/watch?p3XjvQd-1Y4",
+                "حل واجب المحاضرة 2": "https://www.youtube.com/watch?2OxLpga-Wt0",
             },
             3:{
                 tag: ["video", 'video'],
                 title: " محاضرة 3 | مهارات ما قبل التاسيس ",
-                "محاضرة 3 مهارات ما قبل التأسيس": "https://www.youtube.com/embed/6Eaw_WsbU_k?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=8",
-                "حل واجب محاضرة 3": "https://www.youtube.com/embed/druYX1qQpzg?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=9",
+                "محاضرة 3 مهارات ما قبل التأسيس": "https://www.youtube.com/watch?6Eaw_WsbU_k",
+                "حل واجب محاضرة 3": "https://www.youtube.com/watch?druYX1qQpzg",
             },
             4:{
                 tag: ["video"],
                 title: "محاضرة 4 | مهارات ما قبل التأسيس",
-                "محاضرة 4 تعلم التبسيط والإختصارات والقسمة": "https://www.youtube.com/embed/7LUc9PeMNTY?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=11",
+                "محاضرة 4 تعلم التبسيط والإختصارات والقسمة": "https://www.youtube.com/watch?7LUc9PeMNTY",
             },
             5:{
                 tag: ["video"],
                 title: "محاضرة 5 | ايجاد باقي القسمة",
-                "محاضرة 5 باقي القسمة": "https://www.youtube.com/embed/HJrYh3-500A?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=12"
+                "محاضرة 5 باقي القسمة": "https://www.youtube.com/watch?HJrYh3-500A"
             },
             6:{
                 tag: ['test', 'video'],
                 title: "اختبار 1 | الأسبوع الأول",
                 "اختبار الأسبوع 1": "#",
-                "حل اختبار الأسبوع 1": "https://www.youtube.com/embed/OA59-nNKxyY?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=2",
+                "حل اختبار الأسبوع 1": "https://www.youtube.com/watch?OA59-nNKxyY",
             }
         }
     },
@@ -57,39 +57,39 @@ const cards = {
             1:{
                 tag: ["video", 'video'],
                 title: "محاضرة 1 | حساب النسبة والنسبة المئوية",            
-                "محاضرة 1 حساب النسبة والنسبة المئوية": "https://www.youtube.com/embed/RsWFK83EQ5U?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=1",
-                "حل واجب المحاضرة 1": "https://www.youtube.com/embed/4KZVJcCiTzo?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=2",
+                "محاضرة 1 حساب النسبة والنسبة المئوية": "https://www.youtube.com/watch?RsWFK83EQ5U",
+                "حل واجب المحاضرة 1": "https://www.youtube.com/watch?4KZVJcCiTzo",
             },
             2:{
                 tag: ["video", 'video'],
                 title: "محاضرة 2 | تمارين الربح و الخسارة و السعر الاصلي",
-                "محاضرة 2 تمارين الربح و الخسارة و السعر الاصلي": "https://www.youtube.com/embed/VL4r5ojpk7g?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=3",
-                "شرح واجب محاضرة 2": "https://www.youtube.com/embed/8lJ9p5qOkxs?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=4",
+                "محاضرة 2 تمارين الربح و الخسارة و السعر الاصلي": "https://www.youtube.com/watch?VL4r5ojpk7g",
+                "شرح واجب محاضرة 2": "https://www.youtube.com/watch?8lJ9p5qOkxs",
             },
             3:{
                 tag: ["video", 'video'],
                 title: "محاضرة 3 | اجزاء النسب و الضرب التبادلي",
-                "محاضرة 3 اجزاء النسب و الضرب التبادلي": "https://www.youtube.com/embed/nedPAOkORGk?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=5",
-                "حل واجب محاضرة 3": "https://www.youtube.com/embed/w0DUMLELYk4?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=6",
+                "محاضرة 3 اجزاء النسب و الضرب التبادلي": "https://www.youtube.com/watch?nedPAOkORGk",
+                "حل واجب محاضرة 3": "https://www.youtube.com/watch?w0DUMLELYk4",
             },
             4:{
                 tag: ["video", 'video'],
                 title: "محاضرة 4 | الوسط الحسابي",
-                "محاضرة 4 الوسط الحسابي": "https://www.youtube.com/embed/Pc5wPN-3oDE?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=7",
-                "حل واجب محاضرة 4": "https://www.youtube.com/embed/PN3EcUCuw4I?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=8",
+                "محاضرة 4 الوسط الحسابي": "https://www.youtube.com/watch?Pc5wPN-3oDE",
+                "حل واجب محاضرة 4": "https://www.youtube.com/watch?PN3EcUCuw4I",
             },
             5:{
                 tag: ["video", 'video'],
                 title: "محاضرة 5 | الاحصاء و الاحتمالات",
-                "محاضرة 5 الاحصاء و الاحتمالات": "https://www.youtube.com/embed/B64LfpkEck0?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=9",
-                "حل واجب محاضرة 5": "https://www.youtube.com/embed/fMlMPU4hkD0?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=10",
+                "محاضرة 5 الاحصاء و الاحتمالات": "https://www.youtube.com/watch?B64LfpkEck0",
+                "حل واجب محاضرة 5": "https://www.youtube.com/watch?fMlMPU4hkD0",
             },
             6:{
                 tag: ["test", "video", "video"],
                 title: "اختبار 2 , 3 | الأسبوع الثاني",
                 "اختبار 2 | مستوى متوسط ": "#",
-                "حل اختبار 2": "https://www.youtube.com/embed/2VplLG5zrC4?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=2",
-                "حل اختبار 3": "https://www.youtube.com/embed/8Uhy-hXdJtE?controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=en&enablejsapi=1&origin=https%3A%2F%2Ftiqdr.com&widgetid=3",
+                "حل اختبار 2": "https://www.youtube.com/watch?2VplLG5zrC4",
+                "حل اختبار 3": "https://www.youtube.com/watch?8Uhy-hXdJtE",
             }
         }
     },
